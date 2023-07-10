6a21784acc49b7120a307c8fd93d9c6b5792838d
ogamespec <ogamespec@gmail.com>
2023-07-10 13:28:39 +0300

    CellBufferColor = SteelBlue by default

--- Deroute/Properties/Settings.Designer.cs
+++ Deroute/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DerouteSharp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -265,7 +265,7 @@ namespace DerouteSharp.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Navy")]
+        [global::System.Configuration.DefaultSettingValueAttribute("SteelBlue")]
         public global::System.Drawing.Color CellBufferColor {
             get {
                 return ((global::System.Drawing.Color)(this["CellBufferColor"]));
